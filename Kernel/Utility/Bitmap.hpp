5486f866364ceb7efbbd60d1df872730c103cf7e
V1tr10l7 <vitriol1744@gmail.com>
2024-11-23 11:41:15 +0100

    [feat]: implement physical memory allocator

---
+++ Kernel/Utility/Bitmap.hpp
@@ -0,0 +1,42 @@
+/*
+ * Created by v1tr10l7 on 17.11.2024.
+ * Copyright (c) 2024-2024, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include "Utility/Types.hpp"
+
+#include <cstring>
+
+struct Bitmap
+{
+    Bitmap() = default;
+    Bitmap(u8* data, const usize size, const u8 value = 1)
+        : data(data)
+        , size(size)
+    {
+        SetAll(value);
+    }
+
+    inline void SetAll(const u8 value) { memset(data, value, size); }
+    inline void SetIndex(const usize index, const bool value)
+    {
+        const usize byte = index / 8;
+        const usize bit  = index % 8;
+
+        if (value) data[byte] |= Bit(bit);
+        else data[byte] &= ~Bit(bit);
+    }
+    inline bool GetIndex(const usize index) const
+    {
+        const usize byte = index / 8;
+        const usize bit  = index % 8;
+
+        return data[byte] & Bit(bit);
+    }
+
+    u8*   data;
+    usize size;
+};
