5968f4472b843465d98277ba636919e1e35a3f8a
V1tr10l7 <vitriol1744@gmail.com>
2024-12-02 20:05:17 +0100

    [feat]: implement InterruptGuard

---
+++ Kernel/Arch/InterruptGuard.hpp
@@ -0,0 +1,23 @@
+/*
+ * Created by v1tr10l7 on 02.12.2024.
+ * Copyright (c) 2024-2024, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include "Arch/CPU.hpp"
+
+class InterruptGuard final
+{
+  public:
+    InterruptGuard(bool ints)
+        : m_SavedInts(CPU::GetInterruptFlag())
+    {
+        CPU::SetInterruptFlag(ints);
+    }
+    ~InterruptGuard() { CPU::SetInterruptFlag(m_SavedInts); }
+
+  private:
+    bool m_SavedInts;
+};
