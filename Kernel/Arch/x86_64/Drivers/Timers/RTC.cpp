c2c000e1b117e400e6dbba834ebe0d9445fdbe17
V1tr10l7 <vitriol1744@gmail.com>
2025-03-22 22:25:30 +0100

    [fix]: incorrect epoch calculation

--- Kernel/Arch/x86_64/Drivers/Timers/RTC.cpp
+++ Kernel/Arch/x86_64/Drivers/Timers/RTC.cpp
@@ -9,13 +9,16 @@
 #include <Arch/Arch.hpp>
 #include <Arch/x86_64/CMOS.hpp>
 
-#include <utility>
+#include <Firmware/ACPI/ACPI.hpp>
+#include <Prism/Utility/Time.hpp>
 
 namespace RTC
 {
     namespace
     {
-        inline bool IsUpdateInProgress()
+        std::optional<bool> s_FormatInBcd = std::nullopt;
+
+        inline bool         IsUpdateInProgress()
         {
             return CMOS::Read(CMOS::Register::eRtcStatusA) & 0x80;
         }
@@ -28,65 +31,69 @@ namespace RTC
                      ? BcdToBin(value)
                      : value;
         }
-
-        inline bool IsLeapYear(u32 year)
-        {
-            return ((year % 4 == 0)
-                    && ((year % 100 != 0) || (year % 400) == 0));
-        }
-        u32 DaysInMonth(u32 month, u32 year)
-        {
-            switch (month)
-            {
-                case 11: return 30;
-                case 10: return 31;
-                case 9: return 30;
-                case 8: return 31;
-                case 7: return 31;
-                case 6: return 30;
-                case 5: return 31;
-                case 4: return 30;
-                case 3: return 31;
-                case 2:
-                    if (IsLeapYear(year)) return 29;
-                    return 28;
-                case 1: return 31;
-                default: return 0;
-            }
-        }
-
-        unsigned DaysSinceEpoch(u32 year)
-        {
-            u32 days = 0;
-            while (year > 1969)
-            {
-                days += 365;
-                if (IsLeapYear(year)) ++days;
-                --year;
-            }
-            return days;
-        }
     } // namespace
 
     u8 GetCentury()
     {
-        // TODO(V1tri0l1744): Read century from FADT
-        return ReadRegister(CMOS::Register::eRtcCentury);
+        bool centuryAvailable = ACPI::GetCentury();
+        if (!centuryAvailable) return 20;
+
+        auto century = ReadRegister(CMOS::Register::eRtcCentury);
+        return century;
     }
-    u8     GetYear() { return ReadRegister(CMOS::Register::eRtcYear); }
-    u8     GetMonth() { return ReadRegister(CMOS::Register::eRtcMonth); }
-    u8     GetDay() { return ReadRegister(CMOS::Register::eRtcDayOfMonth); }
-    u8     GetHour() { return ReadRegister(CMOS::Register::eRtcHours); }
-    u8     GetMinute() { return ReadRegister(CMOS::Register::eRtcMinutes); }
-    u8     GetSecond() { return ReadRegister(CMOS::Register::eRtcSeconds); }
+    u8 GetYear()
+    {
+        auto year = ReadRegister(CMOS::Register::eRtcYear);
+
+        return year;
+    }
+    u8 GetMonth()
+    {
+        auto month = ReadRegister(CMOS::Register::eRtcMonth);
+
+        return month;
+    }
+    u8 GetDay()
+    {
+        auto day = ReadRegister(CMOS::Register::eRtcDayOfMonth);
+
+        return day;
+    }
+    u8 GetHour()
+    {
+        auto hour = ReadRegister(CMOS::Register::eRtcHours);
+
+        return hour;
+    }
+    u8 GetMinute()
+    {
+        auto minute = ReadRegister(CMOS::Register::eRtcMinutes);
+
+        return minute;
+    }
+    u8 GetSecond()
+    {
+        auto second = ReadRegister(CMOS::Register::eRtcSeconds);
+
+        return second;
+    }
 
     time_t CurrentTime()
     {
-        while (IsUpdateInProgress());
+        while (IsUpdateInProgress())
+            ;
 
-        return DaysSinceEpoch(GetYear() - 1) * 86400
-             + DaysInMonth(GetMonth() - 1, GetYear()) * 86400 + GetDay() * 86400
-             + GetHour() * 3600 + GetMinute() * 60 + GetSecond();
+        if (!s_FormatInBcd.has_value())
+            s_FormatInBcd = !(ReadRegister(CMOS::Register::eRtcStatusB) & 0x04);
+        DateTime date;
+        date.Year   = GetCentury() * 100 + GetYear();
+        date.Month  = GetMonth();
+        date.Day    = GetDay();
+        date.Hour   = GetHour();
+        date.Minute = GetMinute();
+        date.Second = GetSecond();
+
+        return date.ToEpoch();
     }
     u8   GetTime() { return GetHour() * 3600 + GetMinute() * 60 + GetSecond(); }
 
