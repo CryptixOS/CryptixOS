361f8050ce6c3b18279849b8e4b0159a17692d41
V1tr10l7 <vitriol1744@gmail.com>
2025-03-28 18:37:24 +0100

    [refactor]: switch from echfs-utils to parted

--- Meta/import_files.py
+++ Meta/import_files.py
@@ -6,13 +6,13 @@ def process_directory(base_path, root):
         # Create directories first
         relative_path = os.path.relpath(dirpath, base_path)
         if relative_path != ".":
-            subprocess.run(["echfs-utils", "-m", "-p0", "image.hdd", "mkdir", relative_path], check=True)
+            subprocess.run(["echfs-utils", "-m", "-p2", "image.hdd", "mkdir", relative_path], check=True)
             print(f"Created directory: {relative_path}")
         
         # Import files
         for filename in filenames:
             file_relative_path = os.path.join(relative_path, filename)
-            subprocess.run(["echfs-utils", "-m", "-p0", "image.hdd", "import", os.path.join(dirpath, filename), file_relative_path], check=True)
+            subprocess.run(["echfs-utils", "-m", "-p2", "image.hdd", "import", os.path.join(dirpath, filename), file_relative_path], check=True)
             print(f"Imported file: {file_relative_path}")
 
 if __name__ == "__main__":
