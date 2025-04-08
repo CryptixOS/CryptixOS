be30a2ed4186ae7074520ccae2123b191000f25e
V1tr10l7 <vitriol1744@gmail.com>
2025-04-09 00:24:33 +0200

    [fix]: include paths

--- Kernel/Firmware/DeviceTree/Node.hpp
+++ Kernel/Firmware/DeviceTree/Node.hpp
@@ -6,7 +6,7 @@
  */
 #pragma once
 
-#include <Prism/Endian.hpp>
+#include <Prism/Memory/Endian.hpp>
 
 #include <string_view>
 #include <unordered_map>
@@ -32,8 +32,34 @@ namespace DeviceTree
             eEnd       = 9,
         };
 
+        Node(std::string_view name)
+            : m_Name(name)
+        {
+        }
+        void InsertNode(std::string_view name, Node* node)
+        {
+            m_Children[name] = node;
+        }
+        void InsertProperty(std::string_view name, Property* property)
+        {
+            m_Properties[name] = property;
+        }
+
+        void Print(u32 depth = 0)
+        {
+            LogMessage("- {}", m_Name);
+            for (const auto& [name, property] : m_Properties)
+            {
+                for (usize i = 0; i < depth; i++) Logger::LogChar(' ');
+                LogMessage("- {}\n", name);
+            }
+            for (const auto& [name, node] : m_Children)
+                return node->Print(depth + 4);
+        }
+
       private:
-        std::unordered_map<std::string_view, Node*> m_Children;
-        std::unordered_map<std::string_view, Node*> m_Properties;
+        std::string                                     m_Name;
+        std::unordered_map<std::string_view, Node*>     m_Children;
+        std::unordered_map<std::string_view, Property*> m_Properties;
     };
 }; // namespace DeviceTree
