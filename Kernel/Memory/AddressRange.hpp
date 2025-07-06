bad1eaf929f34d56715febfe621e7fd6735a0190
V1tr10l7 <vitriol1744@gmail.com>
2025-07-06 19:51:22 +0200

    [refactor]: make constructor, and other methods of AddressRange constexpr

--- Kernel/Memory/AddressRange.hpp
+++ Kernel/Memory/AddressRange.hpp
@@ -12,20 +12,20 @@
 class AddressRange final
 {
   public:
-    AddressRange() = default;
+    constexpr AddressRange() = default;
 
-    AddressRange(Pointer address, usize size)
+    constexpr AddressRange(Pointer address, usize size)
         : m_Base(address)
         , m_Size(size)
     {
     }
 
-    inline Pointer Base() const { return m_Base; }
-    inline usize   Size() const { return m_Size; }
+    constexpr inline Pointer Base() const { return m_Base; }
+    constexpr inline usize   Size() const { return m_Size; }
 
-    inline         operator bool() { return m_Base.operator bool(); }
+    constexpr inline         operator bool() { return m_Base.operator bool(); }
 
-    inline bool    Contains(Pointer address) const
+    constexpr inline bool    Contains(Pointer address) const
     {
         return address >= m_Base && address < m_Base.Offset<Pointer>(m_Size);
     }
