3c06e78588971287bc434a4fca5a2859af3b4658
V1tr10l7 <vitriol1744@gmail.com>
2025-08-09 18:27:20 +0200

    [refactor]: stop dumping all of the acpi devices to the console for now

---
+++ Kernel/Drivers/ACPI/Bus.hpp
@@ -0,0 +1,14 @@
+/*
+ * Created by v1tr10l7 on 09.08.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include <Compiler.hpp>
+
+namespace ACPI::Bus
+{
+    KERNEL_INIT_SECTION void Initialize();
+}; // namespace ACPI::Bus
---
+++ Kernel/Drivers/ACPI/Driver.hpp
@@ -0,0 +1,18 @@
+/*
+ * Created by v1tr10l7 on 09.08.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include <Library/Locking/Spinlock.hpp>
+#include <Prism/Core/Types.hpp>
+
+namespace ACPI
+{
+    struct Driver
+    {
+        String Name;
+    };
+}; // namespace ACPI
