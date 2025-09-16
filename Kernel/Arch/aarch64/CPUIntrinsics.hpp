24106dba0877bafeefe816a54d0d13708ccebd5b
V1tr10l7 <vitriol1744@gmail.com>
2025-09-17 00:48:17 +0200

    [fix]: fix user access enabler/disabler for aarch64

--- Kernel/Arch/aarch64/CPUIntrinsics.hpp
+++ Kernel/Arch/aarch64/CPUIntrinsics.hpp
@@ -12,6 +12,17 @@
 namespace CPU
 {
 #define Asm(...) __asm__ volatile(__VA_ARGS__)
+#define ReadSystemRegister(register)                                           \
+    ({                                                                         \
+        u64 value;                                                             \
+        Asm("mrs %0, " CtStringify(register) : "=r"(value));                   \
+        value;                                                                 \
+    })
+#define WriteSystemRegister(newValue, register)                                \
+    do {                                                                       \
+        u64 value = static_cast<u64>(newValue);                                \
+        Asm("msr " CtStringify(register) ", %x0" : : "rZ"(value));             \
+    } while (0)
 
     CTOS_ALWAYS_INLINE void Halt() { Asm("msr daifclr, #0b1111; wfi"); }
     CTOS_ALWAYS_INLINE void Pause() { Asm("isb" :: : "memory"); }
@@ -26,17 +37,50 @@ namespace CPU
 
     CTOS_ALWAYS_INLINE void EnableInterrupts() { Asm("msr daifclr, #0b1111"); }
     CTOS_ALWAYS_INLINE void DisableInterrupts() { Asm("msr daifset, #0b1111"); }
-
-    CTOS_ALWAYS_INLINE void EnableUserAccess()
+    CTOS_ALWAYS_INLINE bool ExchangeInterruptFlag(bool enabled)
     {
-        u64 val = 0;
-        Asm("msr pan, %0\n\t"
-            "isb\n\t" ::"r"(val) : "memory");
+        u64 interruptsDisabled;
+        Asm("mrs %0, daif" : "=r"(interruptsDisabled));
+
+        if (enabled) EnableInterrupts();
+        else DisableInterrupts();
+        return !interruptsDisabled;
     }
+
+    constexpr usize         TTBR_ASID_MASK          = 0xffffzu << 48zu;
+    constexpr usize         PAGE_SIZE               = 0x1000;
+    constexpr usize         RESERVED_SWAPPER_OFFSET = PAGE_SIZE;
+
     CTOS_ALWAYS_INLINE void DisableUserAccess()
     {
-        u64 val = 0;
-        Asm("msr pan, %0\n\t"
-            "isb\n\t" ::"r"(val) : "memory");
+        bool irqEnabled = ExchangeInterruptFlag(false);
+        u64  ttbr       = ReadSystemRegister(ttbr1_el1);
+        ttbr &= ~TTBR_ASID_MASK;
+
+        WriteSystemRegister(ttbr - RESERVED_SWAPPER_OFFSET, ttbr0_el1);
+        WriteSystemRegister(ttbr, ttbr1_el1);
+
+        Asm("isb" : : : "memory");
+        ExchangeInterruptFlag(irqEnabled);
+    }
+    CTOS_ALWAYS_INLINE void EnableUserAccess()
+    {
+        /*
+         * Disable interrupts to avoid preemption between reading the 'ttbr0'
+         * variable and the MSR. A context switch could trigger an ASID
+         * roll-over and an update of 'ttbr0'.
+         */
+        bool irqFlag = ExchangeInterruptFlag(false);
+        // FIXME(v1tr10l7): actually read ttbr0 from current thread
+        u64  ttbr0   = 0;
+
+        u64  ttbr1   = ReadSystemRegister(ttbr1_el1);
+        ttbr1 &= ~TTBR_ASID_MASK;
+        ttbr1 |= ttbr0 & TTBR_ASID_MASK;
+        WriteSystemRegister(ttbr1, ttbr1_el1);
+
+        WriteSystemRegister(ttbr0, ttbr0_el1);
+        Asm("isb" : : : "memory");
+        ExchangeInterruptFlag(irqFlag);
     }
 }; // namespace CPU
