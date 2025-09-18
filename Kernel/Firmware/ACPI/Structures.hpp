4fb11ec477a34c55f562070a3c31e77f62b651c9
V1tr10l7 <vitriol1744@gmail.com>
2025-09-18 08:52:04 +0200

    [refactor]: prefix X86BootArchitectureFlags with struct

--- Kernel/Firmware/ACPI/Structures.hpp
+++ Kernel/Firmware/ACPI/Structures.hpp
@@ -186,66 +186,66 @@ namespace ACPI
 
     struct CTOS_PACKED FADT
     {
-        SDTHeader                Header;
-        u32                      FirmwareControl;
-        u32                      DSDT;
-        u8                       Reserved;
-        u8                       PreferredPmProfile;
-        u16                      SciInterrupt;
-        u32                      SmiCmd;
-        u8                       AcpiEnable;
-        u8                       AcpiDisable;
-        u8                       S4BiosReq;
-        u8                       PStateControl;
-        u32                      PM1aEventBlock;
-        u32                      PM1bEventBlock;
-        u32                      PM1aControlBlock;
-        u32                      PM1bControlBlock;
-        u32                      PM2ControlBlock;
-        u32                      PMTimerBlock;
-        u32                      GPE0Block;
-        u32                      GPE1Block;
-        u8                       PM1EventLength;
-        u8                       PM1ControlLength;
-        u8                       PM2ControlLength;
-        u8                       PMTimerLength;
-        u8                       GPE0Length;
-        u8                       GPE1Length;
-        u8                       GPE1Base;
-        u8                       CStateControl;
-        u16                      WorstC2Latency;
-        u16                      WorstC3Latency;
-        u16                      FlushSize;
-        u16                      FlushStride;
-        u8                       DutyOffset;
-        u8                       DutyWidth;
-        u8                       DayAlarm;
-        u8                       MonthAlarm;
-        u8                       Century;
+        SDTHeader                       Header;
+        u32                             FirmwareControl;
+        u32                             DSDT;
+        u8                              Reserved;
+        u8                              PreferredPmProfile;
+        u16                             SciInterrupt;
+        u32                             SmiCmd;
+        u8                              AcpiEnable;
+        u8                              AcpiDisable;
+        u8                              S4BiosReq;
+        u8                              PStateControl;
+        u32                             PM1aEventBlock;
+        u32                             PM1bEventBlock;
+        u32                             PM1aControlBlock;
+        u32                             PM1bControlBlock;
+        u32                             PM2ControlBlock;
+        u32                             PMTimerBlock;
+        u32                             GPE0Block;
+        u32                             GPE1Block;
+        u8                              PM1EventLength;
+        u8                              PM1ControlLength;
+        u8                              PM2ControlLength;
+        u8                              PMTimerLength;
+        u8                              GPE0Length;
+        u8                              GPE1Length;
+        u8                              GPE1Base;
+        u8                              CStateControl;
+        u16                             WorstC2Latency;
+        u16                             WorstC3Latency;
+        u16                             FlushSize;
+        u16                             FlushStride;
+        u8                              DutyOffset;
+        u8                              DutyWidth;
+        u8                              DayAlarm;
+        u8                              MonthAlarm;
+        u8                              Century;
 
         // reserved in ACPI 1.0; used since ACPI 2.0+
-        X86BootArchitectureFlags X86BootArchitectureFlags;
+        struct X86BootArchitectureFlags X86BootArchitectureFlags;
 
-        u8                       Reserved2;
-        FadtFlags                Flags;
+        u8                              Reserved2;
+        FadtFlags                       Flags;
 
         // 12 byte structure; see below for details
-        GenericAddressStructure  ResetReg;
+        GenericAddressStructure         ResetReg;
 
-        u8                       ResetValue;
-        u8                       Reserved3[3];
+        u8                              ResetValue;
+        u8                              Reserved3[3];
 
         // 64bit pointers - Available on ACPI 2.0+
-        u64                      X_FirmwareControl;
-        u64                      X_Dsdt;
+        u64                             X_FirmwareControl;
+        u64                             X_Dsdt;
 
-        GenericAddressStructure  X_PM1aEventBlock;
-        GenericAddressStructure  X_PM1bEventBlock;
-        GenericAddressStructure  X_PM1aControlBlock;
-        GenericAddressStructure  X_PM1bControlBlock;
-        GenericAddressStructure  X_PM2ControlBlock;
-        GenericAddressStructure  X_PMTimerBlock;
-        GenericAddressStructure  X_GPE0Block;
-        GenericAddressStructure  X_GPE1Block;
+        GenericAddressStructure         X_PM1aEventBlock;
+        GenericAddressStructure         X_PM1bEventBlock;
+        GenericAddressStructure         X_PM1aControlBlock;
+        GenericAddressStructure         X_PM1bControlBlock;
+        GenericAddressStructure         X_PM2ControlBlock;
+        GenericAddressStructure         X_PMTimerBlock;
+        GenericAddressStructure         X_GPE0Block;
+        GenericAddressStructure         X_GPE1Block;
     };
 }; // namespace ACPI
